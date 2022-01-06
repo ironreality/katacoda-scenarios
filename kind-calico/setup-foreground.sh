458127ea6f48a7e60416d8abf9f3dcdf92f397d3
Vadym Surzhyk <ironreality@gmail.com>
2022-01-06 12:58:16 +0200

    fix

--- kind-calico/setup-foreground.sh
+++ kind-calico/setup-foreground.sh
@@ -1,1 +1,1 @@
-date; echo "Waiting until Nginx start..."; while ! curl --silent http://localhost > /dev/null; do sleep 5; done; date; echo "Done"
+echo; date; echo "Waiting until Nginx start..."; while ! curl --silent http://localhost > /dev/null; do sleep 5; done; date; echo "Done"
