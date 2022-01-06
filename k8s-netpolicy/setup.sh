8068397a52f4fe844b8161ca4ccbb3e45ac07fa7
Vadym Surzhyk <ironreality@gmail.com>
2022-01-06 12:19:53 +0200

    Fix

--- k8s-netpolicy/setup.sh
+++ k8s-netpolicy/setup.sh
@@ -1,5 +1,6 @@
 ## installing dependencies
-export KUBECONFIG=/etc/kubernetes/admin.conf \
+echo "Waiting for k8s start..."; while [ ! -f /etc/kubernetes/admin.conf ] ; do sleep 2; done; echo \
+&& export KUBECONFIG=/etc/kubernetes/admin.conf \
 && kubectl delete -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml; \
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml \
 && alias k=kubectl \
